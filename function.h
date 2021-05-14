96d534ba490a8c60178dfa16f35cccfcd9e15ea4
ShchukinNY <ShchukinNY@exmaple.com>
2021-05-15 01:09:21 +0300

    добавлен модуль функций

---
+++ function.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+void Input_U(int N, int m, double U[][3]);
+
+void Input_M (int k, int M[20]);
+
+void Output (int L1, int L2, int L3, int N, int k, int M[20], double U[][3], double Z[][20]);
+
+void average_value (int k, int N, double Z[][20]);
+
